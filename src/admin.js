9cc0284c0bda88edbea2b845df29d834839b7e8b
liushuai512 <1570448684@qq.com>
2019-07-25 08:54:25 +0800

    first commit

---
+++ src/admin.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default class Admin extends React.Component{
+
+  render(){
+    return (
+      <Row>
+        <Col span={3}>col-12</Col>
+        <Col span={21}>
+          <Header>
+            Header
+          </Header>
+          <Row>
+            Content
+          </Row>
+          <Footer>
+            Footer
+          </Footer>
+        </Col>
+      </Row>
+    )
+  }
+}
